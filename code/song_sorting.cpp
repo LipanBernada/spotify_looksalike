1dc562befb52ed4b319fa7e08c80583c03f65de1
TheoAaron <100483494+TheoAaron@users.noreply.github.com>
2024-12-03 03:09:44 +0800

    Create song_sorting.cpp

---
+++ code/song_sorting.cpp
@@ -0,0 +1,46 @@
+int durasiLagu(const string& durasi) {
+    int minutes = 0, seconds = 0;
+    char delimiter;
+    stringstream ss(durasi);
+    ss >> minutes >> delimiter >> seconds;
+    return minutes * 60 + seconds;
+}
+
+void sortList(Node*& head, int kriteria) {
+
+    if (!head || !head->next) return;
+    bool swapp;
+
+    do {
+        swapp = false;
+        Node* lagu = head;
+        Node* prev = nullptr;
+
+        while (lagu && lagu->next) {
+            Node* next = lagu->next;
+            bool kondisi = false;
+            if (kriteria == 1) kondisi = lagu->title > next->title;
+            else if (kriteria == 2) kondisi = lagu->artist > next->artist;
+            else if (kriteria == 3) kondisi = lagu->album > next->album;
+            else if (kriteria == 4) {
+                int durasiLagu = durasiLagu(current->durasi);
+                int durasiNext = durasiLagu(next->durasi);
+                kondisi = durasiLagu > durasiNext;
+            }
+
+            if (kondisi) {
+                if (!prev) {
+                    head = next;
+                } else {
+                    prev->next = next;
+                }
+                lagu->next = next->next;
+                next->next = lagu;
+                swapp = true;
+            }
+
+            prev = swapp ? prev : lagu;
+            lagu = lagu->next;
+        }
+    } while (swapp);
+}
